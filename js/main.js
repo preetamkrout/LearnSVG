fcf34edb23aa804f4a683def564320f34ea312b3
Preetam Kajal Rout <preetamkajalrout@gmail.com>
2018-01-16 21:08:16 +0530

    Add cross-browser, touch support and pointer API

--- js/main.js
+++ js/main.js
@@ -4,34 +4,85 @@ let startX = 0,
     startY = 0,
     isDrawing = false;
 
-svgContainer.setAttribute("width", window.innerWidth);
-svgContainer.setAttribute("height", window.innerHeight);
-function drawSign (e) {
-    if (!isDrawing) {
-        return;
+function getPointerLocationForEvent (evt) {
+    let loc = {};
+    if (evt.targetTouches) {
+        loc.offsetX = evt.targetTouches[0].clientX;
+        loc.offsetY = evt.targetTouches[0].clientY;
+    } else {
+        loc.offsetX = evt.clientX;
+        loc.offsetY = evt.clientY;
     }
-    console.log(e.type);
-    let pathD = `${domPath.getAttribute("d")}  L${e.offsetX} ${e.offsetY}`;
-    domPath.setAttribute("d", pathD);
+    return loc;
 }
 
-function penSet (e) {
-    [startX, startY, isDrawing] = [e.offsetX, e.offsetY, true];
-    let dPath = `${domPath.getAttribute("d")} M${startX} ${startY}`;
+function onPenDown (evt) {
+    evt.preventDefault();
+    console.log(evt.type, startX, startY);
+    //To remove multiple touch support
+    if(evt.touches && evt.touches.length > 1) {
+        return;
+    }
+
+    let loc = getPointerLocationForEvent(evt);
+    [startX, startY, isDrawing] = [loc.offsetX, loc.offsetY, true];
+    let dPath = `${domPath.getAttribute("d")}
+    M${startX}, ${startY}`;
     domPath.setAttribute("d", dPath);
+
+    if (window.PointerEvent) {
+        evt.target.setPointerCapture(evt.pointerId);
+    } else {
+        //Add event listener to other mouse events
+        svgContainer.addEventListener("mousemove", onPenMove, true);
+        svgContainer.addEventListener("mouseup", onPenUp, true);
+        svgContainer.addEventListener("mouseleave", onPenUp, true);
+    }
 }
-svgContainer.addEventListener("mousedown", penSet);
-svgContainer.addEventListener("touchstart", penSet);
-svgContainer.addEventListener("mouseup", (e) => {
+
+function onPenMove (evt) {
+    evt.preventDefault();
+    console.log(evt.type, startX, startY);
+    if (!isDrawing) return;
+    let loc = getPointerLocationForEvent(evt);
+    let pathD = `${domPath.getAttribute("d")}
+    L${loc.offsetX}, ${loc.offsetY}`;
+    domPath.setAttribute("d", pathD);
+
+}
+
+function onPenUp (evt) {
+    evt.preventDefault();
+    if (evt.type === "mouseleave" && evt.target.nodeName !== "svg") return;
+    console.log(evt.type, evt.target.nodeName, startX, startY);
     isDrawing = false;
-    console.log(e.type);
-});
-svgContainer.addEventListener("mouseleave", (e) => {
-    isDrawing = false;
-    console.log(e.type);
-});
-svgContainer.addEventListener("touchend", (e) => {
-    isDrawing = false;
-    console.log(e.type);
-});
-svgContainer.addEventListener("mousemove", drawSign);
+    if (window.PointerEvent) {
+        evt.target.releasePointerCapture(evt.pointerId);
+    } else {
+        svgContainer.removeEventListener("mousemove", onPenMove, true);
+        svgContainer.removeEventListener("mouseup", onPenUp, true);
+        svgContainer.removeEventListener("mouseleave", onPenUp, true);
+    }
+}
+
+function main () {  
+    svgContainer.setAttribute("width", window.innerWidth);
+    svgContainer.setAttribute("height", window.innerHeight);
+    if (window.PointerEvent) {
+        //Add pointer events
+        svgContainer.addEventListener("pointerdown", onPenDown, true);
+        svgContainer.addEventListener("pointermove", onPenMove, true);
+        svgContainer.addEventListener("pointerup", onPenUp, true);
+        svgContainer.addEventListener("pointercancel", onPenUp, true);
+    } else {
+        //Touch events
+        svgContainer.addEventListener("touchstart", onPenDown, true);
+        svgContainer.addEventListener("touchmove", onPenMove, true);
+        svgContainer.addEventListener("touchend", onPenUp, true);
+        svgContainer.addEventListener("touchcancel", onPenUp, true);
+        //Mouse start event
+        svgContainer.addEventListener("mousedown", onPenDown, true);
+    }
+}
+
+main();
